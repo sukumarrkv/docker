f5a63f26ff57cf3e41c3f9ad732aef63dddb56be
sukumarrkv <kypa.sukumar3@gmail.com>
2024-02-26 11:33:02 +0530

    Added logs to Controller class

--- springboot-docker/src/main/java/com/example/springbootdocker/controller/DockerController.java
+++ springboot-docker/src/main/java/com/example/springbootdocker/controller/DockerController.java
@@ -1,18 +1,24 @@
 package com.example.springbootdocker.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerController {
 
+	private static final Logger logger = LoggerFactory.getLogger(DockerController.class);
+	
 	@GetMapping("/message")
 	public String message() {
+		logger.info("Getting message in version 1");
 		return "Hello from Spring boot docker application";
 	}
 	
 	@GetMapping("/v2/message")
 	public String messageFromVersion2() {
+		logger.info("Getting message in version 2");
 		return "Hello from Spring boot docker application from version 2";
 	}
 }
