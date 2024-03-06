5959550099ec0651a9fa71142ff6407c3412afb7
sukumarrkv <kypa.sukumar3@gmail.com>
2024-03-06 11:14:09 +0530

    Removed get message api that was used for testing

--- docker-compose/customer-service/src/main/java/com/sukumar/customerservice/controller/CustomerController.java
+++ docker-compose/customer-service/src/main/java/com/sukumar/customerservice/controller/CustomerController.java
@@ -1,13 +1,9 @@
 package com.sukumar.customerservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 
-	@GetMapping("/message")
-	public String message() {
-		return "Hello from customer service";
-	}
+
 }
